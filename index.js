910617c202550f5bbf64389d5901cd0db1dc52e8
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-08 21:23:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var number;
 
 function add(a, b){ 
-  number = a + b;
+  number === a + b;
   return number;
 }
 function subtract(a, b){  
-  number = a - b;
+  number === a - b;
   return number;
 }
 function multiply(a, b){
-  number = a * b;
+  number === a * b;
   return number;
 }
 function divide(a, b){ 
-  number = a / b;
+  number === a / b;
   return number;
 }
 function inc(a){
-  number = a + 1;
+  number === a + 1;
   return number;
 }
 function dec(a){ //Decrement a by 1 and return the result.
-  number = a - 1;
+  number === a - 1;
   return number;
 }
 function makeInt(a){ //Parse a as an integer and return that integer.
